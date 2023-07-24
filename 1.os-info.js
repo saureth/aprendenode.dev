a0ab96fe964acfd9a175263a53a9b3cf1cdc0bc0
Santiago Sanmartín Quintero <santiago.sanmartin@ceiba.com.co>
2023-07-24 10:30:20 -0500

    feat: Logging OS info in consone

---
+++ 1.os-info.js
@@ -0,0 +1,10 @@
+const os = require('node:os');
+
+console.log('Información del sistema operativo');
+console.log('---------------------------------');
+console.log('Nombre: ', os.platform());
+console.log('Versión: ', os.release());
+console.log('Arquitectura: ', os.arch());
+console.log('CPUs', os.cpus());
+console.log('Memoria disponible: ', os.freemem() / 1024/ 1024);
+console.log('Memoria total: ', os.totalmem() / 1024 / 1024);
