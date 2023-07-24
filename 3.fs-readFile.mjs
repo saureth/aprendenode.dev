0a130e9643017d0fd970e7c840771d4f581ccf1e
Santiago Sanmartín Quintero <santiago.sanmartin@ceiba.com.co>
2023-07-24 11:19:25 -0500

    feat: Read two files with synced method

--- 3.fs-readFile.mjs
+++ 3.fs-readFile.mjs
@@ -1,5 +1,12 @@
 //fs: FileSystem
 import fs from 'node:fs';
 
-const text = fs.readFileSync('./fs-utils/hola-mundo.txt','utf-8');
-console.log(text);
+console.log('reading first file ...');
+const firstText = fs.readFileSync('./fs-utils/hola-mundo.txt','utf-8');
+console.log(firstText);
+
+console.log('Must wait to finish reading first file to read next one');
+
+console.log('reading second file ...');
+const secondText = fs.readFileSync('./fs-utils/second-text.txt','utf-8');
+console.log(secondText);
